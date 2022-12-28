2edaa6c6155308e9d2d730316e124ad14624587e
Felipe <felipe1793@hotmail.com>
2022-12-28 18:44:18 -0300

    alteracao para de menu

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-const btnMenu = document.querySelector(".menu") //pegando o botão para fazer o evento
-const btnImg = document.querySelector(".menu img") //Pegando a imagem para alteração
-const sideMenu = document.querySelector(".menu-close") //
+const btnMenu = document.querySelector(".menu") 
+const btnImg = document.querySelector(".menu img") 
+const sideMenu = document.querySelector(".menu-lateral")
+const sideMenuClose = document.querySelector(".menu-lateral img")
 
 const open = () => {
-    if(btnImg.src.indexOf("icon-menu-close.svg") == -1){
-        btnImg.src = "./assets/images/icon-menu-close.svg"
-        sideMenu.classList.remove("menu-close")
-        sideMenu.classList.add("menu-flex")
-    }else {
-        btnImg.src = "./assets/images/icon-menu.svg";
-        sideMenu.classList.remove("menu-flex")
-        sideMenu.classList.add("menu-close")
-    }
-    // btnMenu.innerHTML = "<div class=menu-flex <img src=./assets/images/icon-menu-close.svg> <ol>   <li>Home</li> <li>New</li> <li>Popular</li> <li>Trending</li> <li>Categories</li> </ol> </div>"
-    // for( i=0,)
-    console.log(btnImg.src)
+    btnImg.src = "."
+    sideMenu.classList.add("active")
+}
+const close = () => {
+    btnImg.src = "./assets/images/icon-menu.svg";
+    sideMenu.classList.remove("active");
+    console.log("opa")
 }
 
 btnMenu.addEventListener("click",open)
+sideMenuClose.addEventListener("click",close)
